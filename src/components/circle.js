bd08d544d9a388b00afd055b0e9d050e71fe4030
Sangamesh Mathad <102652901+sangameshmathad@users.noreply.github.com>
2022-05-15 19:25:35 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,11 @@
 // import using require
-
+import * as Shape from ./shape.js;
 // declare class
+export class Circle extends Shape{
+  calculateArea(){
+    console.log("Yo");
+  }
+}
 
 
 // export class using module.exports
